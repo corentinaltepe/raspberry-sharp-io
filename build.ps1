edd89d00e37fcf3f06eae7d0b4ffe1c3f9d18fbe
herman <hmcherman@gmail.com>
2017-07-05 11:17:57 +0800

    Fix nuget cannot pack corrected version
    https://github.com/NuGet/Home/issues/4337

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@ param([String]$Config="Debug")
 
 $gitCommitHash = (git rev-parse HEAD)
 $buildNumber = ([System.DateTime]::UtcNow - [System.DateTime]::Parse("2010-01-01T00:00:00.0000000Z").ToUniversalTime()).TotalDays.ToString("F0")
+$buildNumber += ([System.DateTime]::UtcNow.TimeOfDay.TotalSeconds/10).ToString("0000")
 $VersionSuffix = "build" + $buildNumber
 
 (dotnet msbuild "/t:Restore;Pack" /p:VersionSuffix=$VersionSuffix /p:Configuration=$Config)
